d38137eeb8e65075a78d899e57235c783be78e89
Caleb <beerscb@gmail.com>
2025-03-25 23:39:41 -0400

    UUIDs are default

---
+++ lib/schema.ex
@@ -0,0 +1,14 @@
+defmodule Caque.Schema do
+  @moduledoc """
+  Enforces the use of UUIDs for primary keys and relationships.
+  """
+
+  defmacro __using__(opts \\ []) do
+    quote do
+      use Ecto.Schema
+
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+    end
+  end
+end
