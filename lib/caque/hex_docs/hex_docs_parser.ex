8a3b6741848d39cdd7115ab07f828a28279f9d2a
Dawson <dawsonblack0816@gmail.com>
2025-03-28 12:43:47 -0400

    Making some notes about hexdocs html structure

--- lib/caque/hex_docs/hex_docs_parser.ex
+++ lib/caque/hex_docs/hex_docs_parser.ex
@@ -1,1 +1,5 @@
-####This page is kept intentionally blank
+###Html files have a are structured simply, they have a main div within the body
+###the div has a button, nav, and main. Main is what we focus on.
+
+###body -> div -> main -> div, within that skip the first div which is the search bar
+#within that it's all h tags and p tags with some code, img, and a embeddings
