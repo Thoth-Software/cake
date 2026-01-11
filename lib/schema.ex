7478634bae1c8c6dd5f9403ebb8b1a8687457bf9
Caleb <beerscb@gmail.com>
2026-01-11 17:53:34 -0500

    Schema imports Ecto.Query for query comp

--- lib/schema.ex
+++ lib/schema.ex
@@ -14,6 +14,7 @@ defmodule Cake.Schema do
   defmacro __using__(_opts \\ []) do
     quote do
       use Ecto.Schema
+      import Ecto.Query
 
       @primary_key {:id, :binary_id, autogenerate: true}
       @foreign_key_type :binary_id
