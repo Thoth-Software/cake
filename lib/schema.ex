8c140a7d0d04eb46487b982b2f10eddb4d2e9464
Caleb <beerscb@gmail.com>
2026-04-09 23:15:38 -0400

    Cake.Schema defines a sanitizer function

--- lib/schema.ex
+++ lib/schema.ex
@@ -1,20 +1,29 @@
 defmodule Cake.Schema do
   @moduledoc """
   Enforces the use of UUIDs for primary keys and relationships.
+  Provides shared changeset helpers for all Cake schemas.
   """
 
-  # If we want to add more schemas in the future that are technical docs similar in structure to programming language docs, then this schema is fine as-is
-  # However, if we want to add things with structure very different to technical docs, then we will have to change our approach
-  # Most likely, we will define submodules, Cake.Schema.SubModule, each of which `use`s Cake.Schema and then defines whatever callbacks it defines
-  # Cake.Schema will have attributes common to all schemas
+  import Ecto.Changeset
 
   defmacro __using__(_opts \\ []) do
     quote do
       use Ecto.Schema
       import Ecto.Query
+      import Cake.Schema, only: [sanitize_text_fields: 2]
 
       @primary_key {:id, :binary_id, autogenerate: true}
       @foreign_key_type :binary_id
     end
+  end
+
+  def sanitize_text_fields(changeset, fields) do
+    Enum.reduce(fields, changeset, fn field, cs ->
+      case get_change(cs, field) do
+        nil -> cs
+        value when is_binary(value) -> put_change(cs, field, String.replace(value, <<0>>, ""))
+        _ -> cs
+      end
+    end)
   end
 end
